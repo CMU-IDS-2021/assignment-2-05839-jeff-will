8da21051190412f580bf23bda3a86a023d8eb9a4
WillBorom <70612659+WillBorom@users.noreply.github.com>
2021-03-06 18:35:35 -0500

    Update streamlit_app.py
    Adding in streamline functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,27 @@ chart = alt.Chart(df).mark_point().encode(
 ).interactive()
 
 st.write(chart)"""
+st.title("Do the Rich Have More Time to Visit the Doctor for Potential COVID Cases?📊.")
+
 import altair as alt
 from vega_datasets import data
 import pandas as pd
 import numpy as np
 
 #Load in the datasets------------------------------------------------------------------------
+
+st.write("Let's look at some existing data we have acess to. We have COVID related doctor visits by county as well as general county statistics".)
 COUNTY = pd.read_csv("health_ineq_online_table_12.csv")
 COVID = pd.read_csv("covidcast-doctor-visits-smoothed_adj_cli-2021-01-26-to-2021-02-26.csv")
+st.write(COUNTY.head(5))
+st.write(COVID.head(5))
 #Assign type int to the FIPS County Code we wil join on
 COUNTY["cty"] = COUNTY["cty"].astype(int)
 COVID["geo_value"] = COVID["geo_value"].astype(int)
+
+st.write("Note that there is a column called cty and geo_value in County Data and Covid Data respectively. \
+         This column is a FIPS code that we can use to aggreate data on. \
+         Additionally we also aggreate the COvid data by day using a mean operation.")
 
 #Conduct Inner Join on FIPS County Code
 DATA = COUNTY.join(COVID.set_index("geo_value"), how = "inner", on = "cty")
@@ -45,20 +55,26 @@ DATA = COUNTY.join(COVID.set_index("geo_value"), how = "inner", on = "cty")
 #Aggregate Data by Day, using mean....COVID Doctor Visit Data is daily
 DATA = DATA.groupby("cty").mean().reset_index()
 
-print(DATA.columns)
+st.write(DATA.head(5))
+
 
 #Feature Engineering/ Data Wrangling-----------------------------------------------------------------------
-display(DATA["median_house_value"])
 # transform home value to log scale based on observations
 DATA.median_house_value = DATA.median_house_value.astype(float) 
 #Bin Home value for slider operations
 DATA["binned_home_value_ranges"] = pd.cut(np.log(DATA["median_house_value"]), 10)
 DATA["binned_home_value"] = pd.cut(np.log(DATA["median_house_value"]), 10, labels = False)
 
-display(DATA["median_house_value"])
+st.write("In order to get a meausure of how rich a neighborhood is we use median home value.\
+            We choose to use median home vlaue over income because it is a sronger measure of actual \
+         wealth.")
+
+st.write(DATA["median_house_value"].head(5))
 
 True_Home_Values = []
 
+st.write("In order to get a good idea of how home value is related to COVID realted doctor visits, we first bin the data on home value\
+Note that due to the skewed nature of home value we had to take the log at this point. This is often called feature engineering.")
 
 bins_list = DATA["binned_home_value_ranges"].unique().tolist()
 for i in range (10):
@@ -90,7 +106,7 @@ slider = alt.binding_range(min=0, max=9, step=1)
 select_home_value_level = alt.selection_single(name="binned_home_value", fields=['binned_home_value'],
                                    bind=slider, init={'binned_home_value': 0})
 
-alt.Chart(us_counties).mark_geoshape(
+chart = alt.Chart(us_counties).mark_geoshape(
     stroke='black',
     strokeWidth=0.2
 ).project(
@@ -117,6 +133,9 @@ alt.Chart(us_counties).mark_geoshape(
 ).transform_filter(
     select_home_value_level
 )
+st.write("After conducting the feature engineering and data wrangling, we present to you the following tool. \
+Feel free to use the slider th change the median home value bin. The bin number to actual home values is displayed for your convenience.")
+st.write(chart)
 
 #Displaying Home Values (Bins) for User Transparancey------------------------------------------------------------------
 True_Home_Values = np.reshape(True_Home_Values, (10, 2))
@@ -126,4 +145,6 @@ HOME_VALUES = HOME_VALUES.round(2)
 HOME_VALUES = HOME_VALUES.astype(str)
 HOME_VALUES['Median Home Value Bin Lower'] = '$' + HOME_VALUES['Median Home Value Bin Lower']
 HOME_VALUES['Median Home Value Bin Upper'] = '$' + HOME_VALUES['Median Home Value Bin Upper']
-display(HOME_VALUES)
+
+st.write("For user transparency, the home values by bin came out as follows.")
+st.write(HOME_VALUES)
