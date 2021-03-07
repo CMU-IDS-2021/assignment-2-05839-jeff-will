dcbbdb1ca7307f810706db4fc4e451d158414151
WillBorom <70612659+WillBorom@users.noreply.github.com>
2021-03-06 23:28:04 -0500

    Update streamlit_app.py
    Added more dataset analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,126 +1,200 @@
-"""import streamlit as st
-import pandas as pd
-import altair as alt
-
-st.title("Let's analyze some Penguin Data 🐧📊.")
-
-@st.cache  # add caching so we load the data only once
-def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
-
-df = load_data()
-
-st.write("Let's look at raw data in the Pandas Data Frame.")
-
-st.write(df)
-
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
-
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
-
-st.write(chart)"""
 import altair as alt
 from vega_datasets import data
 import pandas as pd
 import numpy as np
 import streamlit as st
 #Load in the datasets------------------------------------------------------------------------
-st.write("Let's look at some existing data we have acess to. We have COVID related doctor visits by county as well as general county statistics")
-COVID = pd.read_csv("covidcast-doctor-visits-smoothed_adj_cli-2021-01-26-to-2021-02-26.csv")
-COUNTY = pd.read_csv("health_ineq_online_table_12.csv", encoding = "latin-1")
+st.title("Does COVID Affect the Rich Differently than the Poor?")
 
-st.write(COUNTY.head(5))
-st.write(COVID.head(5))
-#Assign type int to the FIPS County Code we wil join on
+st.write("In order to figure this out, we'll look out two measures of wealth and two measures\
+    of COVID effects.")
+
+st.header("Median Home Value vs Percentage of COVID Related Doctor Visits (By County")
+
+st.write("In the graph below, all counties are shown. The higher the color density of the county the higher percentage of doctor\
+    visits that were COVID related (01/26 to 02/26. Additionally, median household income is binned and can be toggled with the slider below.\
+        Play around with the slider to see if you notice any change in color density from high to low home value counties.")
+#Load initial data
+COVID = pd.read_csv("covidcast-doctor-visits-smoothed_adj_cli-2021-01-01-to-2021-01-31.csv")
+COUNTY = pd.read_csv("health_ineq_online_table_12.csv", encoding = "latin-1")
 COUNTY["cty"] = COUNTY["cty"].astype(int)
 COVID["geo_value"] = COVID["geo_value"].astype(int)
-st.write("Note that there is a column called cty and geo_value in County Data and Covid Data respectively. \
-         This column is a FIPS code that we can use to aggreate data on. \
-         Additionally we also aggreate the COvid data by day using a mean operation.")
 #Conduct Inner Join on FIPS County Code
 DATA = COUNTY.join(COVID.set_index("geo_value"), how = "inner", on = "cty")
 #Aggregate Data by Day, using mean....COVID Doctor Visit Data is daily
 DATA = DATA.groupby("cty").mean().reset_index()
-st.write(DATA.head(5))
-#Feature Engineering/ Data Wrangling-----------------------------------------------------------------------
-# transform home value to log scale based on observations
-DATA.median_house_value = DATA.median_house_value.astype(float) 
-#Bin Home value for slider operations
-DATA["binned_home_value_ranges"] = pd.cut(np.log(DATA["median_house_value"]), 10)
-DATA["binned_home_value"] = pd.cut(np.log(DATA["median_house_value"]), 10, labels = False)
-st.write("In order to get a meausure of how rich a neighborhood is we use median home value.\
-            We choose to use median home vlaue over income because it is a sronger measure of actual \
-         wealth.")
-st.write(DATA["median_house_value"].head(5))
-True_Home_Values = []
-st.write("In order to get a good idea of how home value is related to COVID realted doctor visits, we first bin the data on home value\
-Note that due to the skewed nature of home value we had to take the log at this point. This is often called feature engineering.")
-bins_list = DATA["binned_home_value_ranges"].unique().tolist()
-for i in range (10):
-    left_value = np.exp(bins_list[i].left)
-    right_value = np.exp(bins_list[i].right)
-    True_Home_Values.append(left_value)
-    True_Home_Values.append(right_value)
-True_Home_Values = (np.sort(np.asarray(True_Home_Values).astype(float)))
-#Preparatory Operations to conduct Altair map plot----------------------------------------------------------
-#Laod US Counties by FIPS
-us_counties = alt.topo_feature(data.us_10m.url, 'counties')
-#Ensure binned home values are of type int
-DATA['binned_home_value'] = DATA['binned_home_value'].astype(str)
-#Ensure value (which is percent Doctor visits that are covid realted) is float
-DATA['value'] = DATA['value'].astype(float)
-#Pivot the PD Dataframe such that the columns are each bin of home value and rows are indexed by FIPS Code
-DATA_BY_HOME_VALUE = DATA.pivot(index='cty', columns='binned_home_value', values='value').reset_index()
-columns = DATA_BY_HOME_VALUE.columns[1:10].tolist()
-#Altair Map Plot------------------------------------------------------------------------
-slider = alt.binding_range(min=0, max=9, step=1)
-select_home_value_level = alt.selection_single(name="binned_home_value", fields=['binned_home_value'],
-bind=slider, init={'binned_home_value': 0})
-chart = alt.Chart(us_counties).mark_geoshape(
-stroke='black',
-strokeWidth=0.2
-).project(
-type='albersUsa'
-).transform_lookup(
-lookup='id',
-from_=alt.LookupData(DATA_BY_HOME_VALUE, 'cty', columns)
-).transform_fold(
-columns, as_=['binned_home_value', 'value']
-).transform_calculate(
-binned_home_value='parseInt(datum.binned_home_value)',
-value='isValid(datum.value) ? datum.value : 0'  
-).encode(
-tooltip = ['value:Q', 'county_name:N'],
-color = alt.condition(
-'datum.value > 0',
-alt.Color('value:Q', scale=alt.Scale(scheme='reds')),
-alt.value('#FFFFFF')
-    )).add_selection(
-select_home_value_level
-).properties(
-width=700,
-height=400
-).transform_filter(
-select_home_value_level
-)
-st.write("After conducting the feature engineering and data wrangling, we present to you the following tool. \
-Feel free to use the slider th change the median home value bin. The bin number to actual home values is displayed for your convenience.")
+
+def bin_and_log_scale(DATA, column):
+    #Feature Engineering/ Data Wrangling
+    # transform home value to log scale based on observations
+    DATA[column] = DATA[column].astype(float)
+    #Bin Home value for slider operations
+    ranges = column + "_ranges"
+    binned = "binned_col"
+    DATA[ranges] = pd.cut(np.log(DATA[column]), 10)
+    DATA[binned] = pd.cut(np.log(DATA[column]), 10, labels = False).astype(str)
+
+
+    True_Values = []
+
+
+    bins_list = DATA[ranges].unique().tolist()
+    for i in range (10):
+        left_value = np.exp(bins_list[i].left)
+        right_value = np.exp(bins_list[i].right)
+        True_Values.append(left_value)
+        True_Values.append(right_value)
+
+    True_Values = (np.sort(np.asarray(True_Values).astype(float)))
+                        
+    return DATA, True_Values
+
+DATA, True_Home_Values = bin_and_log_scale(DATA, "median_house_value")
+
+def create_map(DATA_MAP, FIPS_NAME, binned_col, value_col, SCHEME):
+    
+    DATA_MAP[value_col] = DATA_MAP[value_col].astype(float)
+    #Laod US Counties by FIPS
+    us_counties = alt.topo_feature(data.us_10m.url, 'counties')
+
+    #Pivot the PD Dataframe such that the columns are each bin of home value and rows are indexed by FIPS Code
+    DATA_MAP = DATA_MAP.pivot(index=FIPS_NAME, columns=binned_col, values=value_col).reset_index()
+    columns = DATA_MAP.columns[1:10].tolist()
+
+    #Create the slider
+    slider = alt.binding_range(min=0, max=9, step=1)
+    select_home_value_level = alt.selection_single(name=binned_col, fields=[binned_col],
+                                       bind=slider, init={binned_col: 0})
+    #Create the chart
+    chart = alt.Chart(us_counties).mark_geoshape(
+        stroke='black',
+        strokeWidth=0.2
+    ).project(
+        type='albersUsa'
+    ).transform_lookup(
+        lookup='id',
+        from_=alt.LookupData(DATA_MAP, FIPS_NAME, columns)
+    ).transform_fold(
+        columns, as_=[binned_col, 'value']
+    ).transform_calculate(
+        binned_col='parseInt(datum.binned_col)',
+        value='isValid(datum.value) ? datum.value : 0'  
+    ).encode(
+        tooltip = ['value:Q', 'county_name:N'],
+        color = alt.condition(
+            'datum.value > 0',
+            alt.Color('value:Q', scale=alt.Scale(scheme=SCHEME, domain=[0, 50])),
+            alt.value('#FFFFFF')
+        )).add_selection(
+        select_home_value_level
+    ).properties(
+        width=700,
+        height=400
+    ).transform_filter(
+        select_home_value_level
+    )
+    
+    return chart
+
+chart = create_map(DATA, "cty", "binned_col", "value", "greens")
 st.write(chart)
-#Displaying Home Values (Bins) for User Transparancey------------------------------------------------------------------
+
+st.write("At first it may seem like there is no noticeable effect. There is one interesting \
+    observation that the home values in the middle of the range seem to have very low COVID doctor visits\
+        while the richest and the poorest have the highest COVID Doctor visits. This may be the result of 2 phenomenon. The\
+            rich have time to visit the docor more for COVID while the poor are still more likely to be affected by it.")
+
+
 True_Home_Values = np.reshape(True_Home_Values, (10, 2))
-HOME_VALUES = pd.DataFrame(True_Home_Values, columns = ['Median Home Value Bin Lower', 'Median Home Value Bin Upper'])
+TRUE_HOME_VALUES = pd.DataFrame(True_Home_Values, columns = ['Median Home Value Bin Lower', 'Median Home Value Bin Upper'])
 #pd.options.display.float_format = "{:,.2f}".format
-HOME_VALUES = HOME_VALUES.round(2)
-HOME_VALUES = HOME_VALUES.astype(str)
-HOME_VALUES['Median Home Value Bin Lower'] = '$' + HOME_VALUES['Median Home Value Bin Lower']
-HOME_VALUES['Median Home Value Bin Upper'] = '$' + HOME_VALUES['Median Home Value Bin Upper']
-st.write("For user transparency, the home values by bin came out as follows.")
-st.write(HOME_VALUES)
+TRUE_HOME_VALUES = TRUE_HOME_VALUES.round(2)
+TRUE_HOME_VALUES = TRUE_HOME_VALUES.astype(str)
+TRUE_HOME_VALUES['Median Home Value Bin Lower'] = '$' + TRUE_HOME_VALUES['Median Home Value Bin Lower']
+TRUE_HOME_VALUES['Median Home Value Bin Upper'] = '$' + TRUE_HOME_VALUES['Median Home Value Bin Upper']
+st.write(TRUE_HOME_VALUES)
+
+
+st.write("Let's consider another measure of wealth, median income, and see if we notice the same phenomenon.")
+
+st.header("Median Income vs Percentage of COVID Related Doctor Visits (By County)")
+
+COUNTY2 = pd.read_csv("cty_covariates.csv", encoding = "latin1")
+#This data set does not come with FIPS so we have to create it by appending state and county FIPS
+COUNTY2["FIPS"] = (COUNTY2["state"]*1000 + COUNTY2["county"]).astype(int)
+#Conduct Inner Join on FIPS County Code
+DATA = COUNTY2.join(COVID.set_index("geo_value"), how = "inner", on = "FIPS")
+
+#Aggregate Data by Day, using mean....COVID Doctor Visit Data is daily
+DATA = DATA.groupby("FIPS").mean().reset_index()
+
+DATA, True_Inc_Values = bin_and_log_scale(DATA, "hhinc_mean2000")
+
+chart = create_map(DATA, "FIPS", "binned_col", "value", "greens")
+st.write(chart)
+st.write("Again, it seems like the counties with the highest percentage of COVID Doctor visits are both the poorest and richest counties.")
+
+True_Inc_Values = np.reshape(True_Inc_Values, (10, 2))
+INC_VALUES = pd.DataFrame(True_Inc_Values, columns = ['Median Income Bin Lower', 'Median Income Bin Upper'])
+#pd.options.display.float_format = "{:,.2f}".format
+INC_VALUES = INC_VALUES.round(2)
+INC_VALUES = INC_VALUES.astype(str)
+INC_VALUES['Median Income Bin Lower'] = '$' + INC_VALUES['Median Income Bin Lower']
+INC_VALUES['Median Income Bin Upper'] = '$' + INC_VALUES['Median Income Bin Upper']
+st.write(INC_VALUES)
+
+st.write("Perhaps percentage of COVID Doctor Visits is a bit obfuscated. Let's look at a more grim measure, COVID related Deaths.")
+
+st.header("Median Home Value vs Percentage of COVID Deaths (By County")
+
+COVID = pd.read_csv("covidcast-doctor-visits-smoothed_adj_cli-2021-01-01-to-2021-01-31.csv")
+COUNTY = pd.read_csv("health_ineq_online_table_12.csv", encoding = "latin-1")
+COUNTY["cty"] = COUNTY["cty"].astype(int)
+COVID["geo_value"] = COVID["geo_value"].astype(int)
+
+#Conduct Inner Join on FIPS County Code
+DATA = COUNTY.join(COVID.set_index("geo_value"), how = "inner", on = "cty")
+
+#Aggregate Data by Day, using mean....COVID Doctor Visit Data is daily
+DATA = DATA.groupby("cty").mean().reset_index()
+
+DATA, True_Home_Values = bin_and_log_scale(DATA, "median_house_value")
+
+chart = create_map(DATA, "cty", "binned_col", "value", "reds")
+st.write(chart)
+
+True_Home_Values = np.reshape(True_Home_Values, (10, 2))
+TRUE_HOME_VALUES = pd.DataFrame(True_Home_Values, columns = ['Median Home Value Bin Lower', 'Median Home Value Bin Upper'])
+#pd.options.display.float_format = "{:,.2f}".format
+TRUE_HOME_VALUES = TRUE_HOME_VALUES.round(2)
+TRUE_HOME_VALUES = TRUE_HOME_VALUES.astype(str)
+TRUE_HOME_VALUES['Median Home Value Bin Lower'] = '$' + TRUE_HOME_VALUES['Median Home Value Bin Lower']
+TRUE_HOME_VALUES['Median Home Value Bin Upper'] = '$' + TRUE_HOME_VALUES['Median Home Value Bin Upper']
+st.write(TRUE_HOME_VALUES)
+
+st.write("Wow! This one is even more even out throughout the ranks of society. Let's look at how COVID realted deaths realte to median income.")
+st.header("Median Income vs Percentage of COVID Deaths (By County)")
+
+#This data set does not come with FIPS so we have to create it by appending state and county FIPS
+COUNTY2["FIPS"] = (COUNTY2["state"]*1000 + COUNTY2["county"]).astype(int)
+#Conduct Inner Join on FIPS County Code
+DATA = COUNTY2.join(COVID.set_index("geo_value"), how = "inner", on = "FIPS")
+
+#Aggregate Data by Day, using mean....COVID Doctor Visit Data is daily
+DATA = DATA.groupby("FIPS").mean().reset_index()
+
+DATA, True_Inc_Values = bin_and_log_scale(DATA, "hhinc_mean2000")
+
+
+chart = create_map(DATA, "FIPS", "binned_col", "value", "reds")
+st.write(chart)
+
+
+True_Inc_Values = np.reshape(True_Inc_Values, (10, 2))
+INC_VALUES = pd.DataFrame(True_Inc_Values, columns = ['Median Income Bin Lower', 'Median Income Bin Upper'])
+#pd.options.display.float_format = "{:,.2f}".format
+INC_VALUES = INC_VALUES.round(2)
+INC_VALUES = INC_VALUES.astype(str)
+INC_VALUES['Median Income Bin Lower'] = '$' + INC_VALUES['Median Income Bin Lower']
+INC_VALUES['Median Income Bin Upper'] = '$' + INC_VALUES['Median Income Bin Upper']
+st.write(INC_VALUES)
