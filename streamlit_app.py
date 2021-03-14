57a1e3ae561d8352bffcaa32882f5b78d64d52c3
WillBorom <70612659+WillBorom@users.noreply.github.com>
2021-03-14 19:30:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,41 @@ def load_data():
     DATA = COUNTY.join(COVID.set_index("geo_value"), how = "inner", on = "cty")
     DATA['Date'] = pd.to_datetime(DATA.time_value)
     DATA['Date'] = DATA.Date.dt.strftime('%d').astype(int)
-    return DATA
+    return DATA.sample(100)
 
 DATA = load_data()
-    
+
+st.title("% of Doctor Visits by State and County")
+st.write("In this section, we explore the percentage of doctor visits for COVID by State and County.  We begin by hilighting Pennsylvania and as we can see, there are some interesting observatins for <> county.  As you probably noticed, there looks to be a correlation between ")
+
+alt.data_transformers.disable_max_rows()
+
+slider = alt.binding_range(min=1, max=31, step=1)
+select_date = alt.selection_single(name="January", fields=['Date'], bind=slider, init={'Date':1})
+
+state_selector = alt.selection_multi(fields=['statename'], init=[{'statename':'Pennsylvania'}])
+
+States = alt.Chart(DATA).mark_bar().encode(
+    x=alt.X('value:Q', title="% of Visits to Doctor about COVID", aggregate="mean", scale=alt.Scale(domain=[0, 35])),
+    y=alt.Y('statename:N', title="State"),
+    color=alt.condition(state_selector, alt.value("#f76f5c"), alt.value("#451076")),
+    tooltip=[alt.Tooltip("statename:N", title='State'), alt.Tooltip("value:Q", aggregate="mean", title="% of COVID Doctor Visits", format='.2f')]
+    ).add_selection(
+        state_selector
+    ).add_selection(
+        select_date
+    ).transform_filter(
+        select_date).interactive()
+
+Counties = alt.Chart(DATA).mark_bar(color='#451076').encode(
+    x=alt.X('value:Q', title="% of Visits to Doctor about COVID", scale=alt.Scale(domain=[0,35])),
+    y=alt.Y('county_name:N', title = "County"),
+    tooltip=[alt.Tooltip("statename:N", title='State'), alt.Tooltip("county_name:N", title='County'),alt.Tooltip("value:Q", aggregate="mean", title="% of COVID Doctor Visits", format='.2f'), alt.Tooltip("puninsured2010:Q", aggregate="mean", title="% Uninsured (as of 2010)", format='.2f')]
+    ).transform_filter(
+        state_selector & select_date).interactive()
+joint_chart = States | Counties
+st.write(joint_chart)
+
 
 st.header("Geographical Representation")
 
